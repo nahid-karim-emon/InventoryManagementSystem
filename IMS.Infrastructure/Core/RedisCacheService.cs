0303ae05ca84a429d655ad13863d3f66ff7d3f67
Nahid Karim Emon <nahid.emon@technonext.com>
2026-01-03 18:13:45 +0600

    Add Redis in Product

---
+++ IMS.Infrastructure/Core/RedisCacheService.cs
@@ -0,0 +1,45 @@
+﻿using IMS.Core.Caching;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace IMS.Infrastructure.Core
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisCacheService(IDistributedCache cache)
+        {   
+            _cache = cache;
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var jsonData = await _cache.GetStringAsync(key);
+
+            if (jsonData is null)
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+         
+        public async Task RemoveAsync(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+            if (expiry.HasValue)
+            {
+                options.AbsoluteExpirationRelativeToNow = expiry;
+            }
+
+            var jsonData = JsonSerializer.Serialize(value);
+            await _cache.SetStringAsync(key, jsonData, options);
+        }
+    }
+}
